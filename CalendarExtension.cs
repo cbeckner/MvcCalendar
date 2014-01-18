9f4033b995a6692ca9e8f1e18f5b236f9c4a4968
Cody Beckner <cody.beckner@gmail.com>
2014-01-18 16:17:01 +0000

    Updated HTML output to be more compliant.

--- CalendarExtension.cs
+++ CalendarExtension.cs
@@ -21,31 +21,33 @@ namespace XYZ.CalendarHelper
 
         public static MvcHtmlString Calendar(this HtmlHelper helper, DateTime monthToRender, List<DateTime> blockedDates)
         {
-            return Calendar(helper, monthToRender, blockedDates,null);
+            return Calendar(helper, monthToRender, blockedDates, null);
         }
 
         public static MvcHtmlString Calendar(this HtmlHelper helper, DateTime monthToRender, List<DateTime> blockedDates, object htmlAttributes)
         {
             TagBuilder calendar = new TagBuilder("table");
             calendar.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
-            
+
             if (blockedDates == null) blockedDates = new List<DateTime>();
             blockedDates = blockedDates.Select(d => d.Date).ToList();
 
-            TagBuilder header = new TagBuilder("tr");
-            TagBuilder monthName = new TagBuilder("td");
+            TagBuilder monthName = new TagBuilder("caption");
             monthName.Attributes.Add("style", "font-weight:bold");
             monthName.Attributes.Add("class", "monthname");
-            monthName.Attributes.Add("colspan", "7");
             monthName.SetInnerText(monthToRender.ToString("MMMM"));
-            header.InnerHtml = monthName.ToString();
+            calendar.InnerHtml = monthName.ToString();
 
             //Build Day Names
             TagBuilder dayNames = new TagBuilder("tr");
-            var days = new [] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"};
-            for(int i = 0;i<7;i++) {
+            var days = new[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+            var daysLong = new[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            for (int i = 0; i < 7; i++)
+            {
                 TagBuilder dayHeader = new TagBuilder("th");
-                dayHeader.SetInnerText(days[i]);
+                dayHeader.Attributes.Add("abbr", days[i]);
+                dayHeader.Attributes.Add("title", daysLong[i]);
+                dayHeader.SetInnerText(days[i].ToUpper());
                 dayNames.InnerHtml += dayHeader.ToString();
             }
 
@@ -53,7 +55,7 @@ namespace XYZ.CalendarHelper
             List<TagBuilder> monthRows = new List<TagBuilder>();
             TagBuilder row = new TagBuilder("tr");
             int columnCount = 0;
-            for (int i = 1; i <= DateTime.DaysInMonth(monthToRender.Year, monthToRender.Month);i++ )
+            for (int i = 1; i <= DateTime.DaysInMonth(monthToRender.Year, monthToRender.Month); i++)
             {
                 DateTime day = new DateTime(monthToRender.Year, monthToRender.Month, i);
                 if (i == 1)
@@ -90,12 +92,12 @@ namespace XYZ.CalendarHelper
             }
             monthRows.Add(row);
 
-            calendar.InnerHtml = header.ToString();
             calendar.InnerHtml += dayNames.ToString();
-            foreach(var daysrow in monthRows) {
+            foreach (var daysrow in monthRows)
+            {
                 calendar.InnerHtml += daysrow.ToString();
             }
-            return MvcHtmlString.Create(calendar.ToString(TagRenderMode.Normal)); 
+            return MvcHtmlString.Create(calendar.ToString(TagRenderMode.Normal));
         }
     }
 }
